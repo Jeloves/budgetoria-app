bd75ce144d998650ff38eb381beae20bffb60c4e
Jeloves <anjelovaleraecube@icloud.com>
2024-03-13 19:12:15 -0400

    Created GET functions for Firebase-Firestore (getBudgets, getAllocations)

--- pages/budget/index.tsx
+++ pages/budget/index.tsx
@@ -3,9 +3,13 @@ import "@/styles/global.scss";
 import { useEffect, useState } from "react";
 import { auth, getUser } from "@/firebase/auth";
 import { User } from "firebase/auth/cordova";
+import { getSelectedBudget } from "@/firebase/budgets";
+import { getAllocations } from "@/firebase/allocations";
+import { Allocation, Budget } from "@/firebase/models";
 
 export default function BudgetPage() {
 	const [user, setUser] = useState<User | null>(null);
+	const [budget, setBudget] = useState<Budget | null>(null);
 
 	// Sets user
 	useEffect(() => {
@@ -16,7 +20,12 @@ export default function BudgetPage() {
 
 	// Sets budgets
 	useEffect(() => {
-		const fetchBudgetData = async () => {};
+		const fetchBudgetData = async () => {
+			if (user) {
+				const data = await getSelectedBudget(user.uid);
+				setBudget(data);
+			}
+		};
 		fetchBudgetData();
 	}, [user]);
 
