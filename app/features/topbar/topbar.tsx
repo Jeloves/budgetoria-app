62888098f153845611cbfbeefd24c980284cbef7
Jeloves <anjelovaleraecube@icloud.com>
2024-03-13 18:46:40 -0400

    Added dynamic code for creating <td> elements, with onClicks to change month

--- app/features/topbar/topbar.tsx
+++ app/features/topbar/topbar.tsx
@@ -12,40 +12,67 @@ export function Topbar() {
 
 	const handleDateDisplayOnClick = () => {
 		setIsDatePickerShowing(!isDatePickerShowing);
-	}
+	};
+
+	const handleSideNavigationOnClick = () => {
+		alert("Show sidenav");
+	};
+
+	const handleEditOnClick = () => {
+		alert("Edit categories");
+	};
+
+	const handleCreateTransactionOnClick = () => {
+		alert("Create new transaction");
+	};
+
+	const handlePreviousYearOnClick = () => {};
+
+	const handleNextYearOnClick = () => {};
+
+	const handleMonthOnClick = (index: number) => {
+		setMonth(index);
+		setIsDatePickerShowing(false);
+	};
 
 	const dateDisplay = (
 		<div className={styles.dateDisplay}>
-			<span>Mar 2024</span>
-			<IconButton button={{onClick: handleDateDisplayOnClick}} src="/icons/arrow-down.svg" altText="Button to open Date Picker"/>
+			<span>
+				{monthAcronyms[month]} {year}
+			</span>
+			<IconButton button={{ onClick: handleDateDisplayOnClick }} src="/icons/arrow-down.svg" altText="Button to open Date Picker" />
 		</div>
 	);
+
+	let trArray: JSX.Element[] = [];
+	let tdArray: JSX.Element[] = [];
+	monthAcronyms.map((acronym, index) => {
+		tdArray.push(
+			<td
+				key={index}
+				onClick={() => {
+					handleMonthOnClick(index);
+				}}
+			>
+				{acronym}
+			</td>
+		);
+		if (index === 3 || index === 7 || index === 11) {
+			trArray.push(<tr>{tdArray}</tr>);
+			tdArray = [];
+		}
+	});
 
 	const datePicker = (
 		<section className={styles.datePicker}>
 			<header>
+				<IconButton button={{ onClick: handlePreviousYearOnClick }} src="/icons/arrow-left.svg" altText="Button to select previous year" />
 				<h1>2024</h1>
+				<IconButton button={{ onClick: handleNextYearOnClick }} src="/icons/arrow-right.svg" altText="Button to select next year" />
 			</header>
 			<table>
 				<tbody>
-					<tr>
-						<td>Jan</td>
-						<td>Feb</td>
-						<td>Mar</td>
-						<td>Apr</td>
-					</tr>
-					<tr>
-						<td>May</td>
-						<td>Jun</td>
-						<td>Jul</td>
-						<td>Aug</td>
-					</tr>
-					<tr>
-						<td>Sep</td>
-						<td>Oct</td>
-						<td>Nov</td>
-						<td>Dec</td>
-					</tr>
+					{trArray}
 				</tbody>
 			</table>
 		</section>
@@ -54,10 +81,10 @@ export function Topbar() {
 	return (
 		<>
 			<section className={styles.topbarContainer}>
-				<IconButton src="/icons/ellipsis.svg" altText="Button to open Side Navigation" />
+				<IconButton button={{ onClick: handleSideNavigationOnClick }} src="/icons/ellipsis.svg" altText="Button to open Side Navigation" />
 				{dateDisplay}
-				<IconButton src="/icons/edit.svg" altText="Button to edit categories" />
-				<IconButton src="/icons/circled-dollar.svg" altText="Button to create new transaction" />
+				<IconButton button={{ onClick: handleEditOnClick }} src="/icons/edit.svg" altText="Button to edit categories" />
+				<IconButton button={{ onClick: handleCreateTransactionOnClick }} src="/icons/circled-dollar.svg" altText="Button to create new transaction" />
 			</section>
 			{isDatePickerShowing && datePicker}
 		</>
