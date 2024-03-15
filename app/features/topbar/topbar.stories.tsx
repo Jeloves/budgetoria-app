0410b31b3df6b3369847dd09f8c8f6a76a0005f9
Jeloves <anjelovaleraecube@icloud.com>
2024-03-15 15:29:28 -0400

    Fixing deployment

--- app/features/topbar/topbar.stories.tsx
+++ app/features/topbar/topbar.stories.tsx
@@ -13,3 +13,11 @@ export default {
 const Template: StoryFn<TopbarPropsType> = (args) => <Topbar {...args}/>;
 
 export const Default = Template.bind({});
+
+Default.args = {
+  month: 2,
+  year: 2024,
+  handleDateChangeOnClick: (monthIndex: number, year: number) => {
+    alert("date changed")
+  },
+}
