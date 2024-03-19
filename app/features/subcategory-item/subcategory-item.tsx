61f93e438baac975e40c7a8f633e4dc5fe2fa311
Jeloves <anjelovaleraecube@icloud.com>
2024-03-19 17:44:43 -0400

    CategoryItem & SubcategoryItem allocations are updated via inputs

--- app/features/subcategory-item/subcategory-item.tsx
+++ app/features/subcategory-item/subcategory-item.tsx
@@ -1,27 +1,60 @@
 import classNames from "classnames";
 import styles from "./subcategory-item.module.scss";
+import { useState } from "react";
 
 export type SubcategoryItemPropsType = {
 	name: string;
 	currencyString: string;
 	assigned: number;
 	available: number;
+	updateCategoryAllocations: (changeInAssignedValue: number) => void;
 };
 
 export function SubcategoryItem(props: SubcategoryItemPropsType) {
-	const { name, currencyString, assigned, available } = props;
+	const { name, currencyString, assigned, available, updateCategoryAllocations } = props;
+	const [assignedAllocation, setAssignedAllocation] = useState<number>(assigned);
+	const [availableAllocation, setAvailableAllocation] = useState<number>(available);
+	const [key, setKey] = useState<number>(0);
+
+	const handleEnterKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === "Enter") {
+			event.currentTarget.blur();
+		}
+	};
+
+	const handleInputBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+		let value = event.target.value;
+		// Removes currency string if present
+		if (value.includes(currencyString)) {
+			value = value.replace(currencyString, "");
+		}
+		// Removes non-number characters
+		const nonCurrencyRegex = /[^0-9.]/g;
+		value = value.replace(nonCurrencyRegex, "");
+
+		const isValidNumber = !isNaN(parseFloat(value));
+		const newAssigned = isValidNumber ? parseFloat(value) * 1000000 : 0;
+		const oldAssigned = assignedAllocation;
+		const changeInAssignedValue = newAssigned - oldAssigned;
+
+		if (changeInAssignedValue !== 0) {
+			setAssignedAllocation(newAssigned);
+			setAvailableAllocation(availableAllocation + changeInAssignedValue)
+			updateCategoryAllocations(changeInAssignedValue);
+		}
+		
+		// Used to re-render this component
+		setKey(key + 1);
+	};
 
 	return (
 		<section className={styles.subcategory}>
 			<span className={styles.subcategoryName}>{name}</span>
-			<div className={classNames(styles.allocation)}>
-				{currencyString}
-				<input type="text" value={(assigned / 1000000).toFixed(2)}/>
-			</div>
+			<input className={styles.input} type="text" defaultValue={currencyString + (assignedAllocation / 1000000).toFixed(2)} onBlur={handleInputBlur} onKeyDown={handleEnterKeyDown} key={key} />
 			<div className={classNames(styles.allocation)}>
 				<span>
 					{currencyString}
-					{(available / 1000000).toFixed(2)}
+					{(availableAllocation / 1000000).toFixed(2)}
 				</span>
 			</div>
 		</section>
