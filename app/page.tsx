88f8c4b9ec108c3de5df42c57f54766b6c6bbd85
Jeloves <anjelovaleraecube@icloud.com>
2024-03-29 11:41:41 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-"use client";
 import styles from "./page.module.scss";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { getUser } from "@/firebase/auth";
 
 export default function Home() {
-	const router = useRouter();
+  
 	if (getUser()) {
-		router.push("/budget");
+		redirect("/budget");
 	} else {
-    router.push("/login")
-  }
+		redirect("/login");
+	}
 
 	return (
 		<main className={styles.main}>
