eda7827856299bc5ecd09d9fa401df77ce299a70
Jeloves <anjelovaleraecube@icloud.com>
2024-04-16 16:54:50 -0400

    Deleted unused file

--- app/features/edit-page/edit-pageEditPage.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { EditPage } from './edit-page'
-
-describe('<EditPage />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<EditPage />)
-  })
-})
