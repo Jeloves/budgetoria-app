dc38a729851394fb9c8b4cc2102e15a46f6278b3
Jeloves <anjelovaleraecube@icloud.com>
2024-04-25 06:47:59 -0400

    Added props to accounts-selection-subpage.stories

--- app/features/transaction-page/account-selection-subpage/account-selection-subpage.stories.tsx
+++ app/features/transaction-page/account-selection-subpage/account-selection-subpage.stories.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import { AccountSelectionSubpage, AccountSelectionSubpagePropsType } from "./account-selection-subpage";
 import { v4 as uuidv4 } from "uuid";
+import { Account } from "@/firebase/models";
+import { getAccountNameByID } from "@/utils/getByID";
 
 
 export default {
@@ -16,5 +18,16 @@ const Template: StoryFn<AccountSelectionSubpagePropsType> = (args) => <AccountSe
 
 export const Default = Template.bind({});
 
+const account1 = new Account(uuidv4(), "Checkings", 2000000000, 1800000000);
+const account2 = new Account(uuidv4(), "Credit", 0, -10000000);
+const account3 = new Account(uuidv4(), "Savings", 347880000, 347880000);
+const accounts = [account1, account2, account3];
+
 Default.args = {
+    selectedAccountID: account1.id,
+    accounts: accounts,
+    handleBackClick: () => {alert("Back to transaction page")},
+    selectAccount: (selectedAccountID: string) => {
+        alert(`Selected account: ${getAccountNameByID(selectedAccountID, accounts)}`);
+    }
 };
