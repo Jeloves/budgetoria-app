be6b2182869555f06e237e485ff38f2634110954
Jeloves <anjelovaleraecube@icloud.com>
2024-04-30 15:47:32 -0400

    Added date parameters to all Account constructors

--- app/features/transaction-page/account-selection-subpage/account-selection-subpage.stories.tsx
+++ app/features/transaction-page/account-selection-subpage/account-selection-subpage.stories.tsx
@@ -4,6 +4,7 @@ import { AccountSelectionSubpage, AccountSelectionSubpagePropsType } from "./acc
 import { v4 as uuidv4 } from "uuid";
 import { Account } from "@/firebase/models";
 import { getAccountNameByID } from "@/utils/getByID";
+import { Timestamp } from "firebase/firestore";
 
 
 export default {
@@ -18,9 +19,9 @@ const Template: StoryFn<AccountSelectionSubpagePropsType> = (args) => <AccountSe
 
 export const Default = Template.bind({});
 
-const account1 = new Account(uuidv4(), "Checkings", 2000000000, 1800000000);
-const account2 = new Account(uuidv4(), "Credit", 0, -10000000);
-const account3 = new Account(uuidv4(), "Savings", 347880000, 347880000);
+const account1 = new Account(uuidv4(), "Checkings", Timestamp.fromDate(new Date(2024, 0, 1)), 2000000000, 1800000000);
+const account2 = new Account(uuidv4(), "Credit", Timestamp.fromDate(new Date(2024, 0, 1)), 0, -10000000);
+const account3 = new Account(uuidv4(), "Savings", Timestamp.fromDate(new Date(2024, 0, 1)), 347880000, 347880000);
 const accounts = [account1, account2, account3];
 
 Default.args = {
