4f05b49d4413b9ad0db02eac9af08e2c14312d3f
Jeloves <anjelovaleraecube@icloud.com>
2024-05-07 13:01:58 -0400

    Recording problems

--- cypress/e2e/budget-page/budget-page.cy.ts
+++ cypress/e2e/budget-page/budget-page.cy.ts
@@ -17,4 +17,9 @@ describe("Budget Page", () => {
 		//connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
     //connectAuthEmulator(auth, "http://127.0.0.1:9099");
 	});
+
+	// TODO - figure out how to connect to auth emulator from within Cypress tests
+	// Current Problems
+	///		- when placed at the last line of their respective files, these connectEmulator() functions work
+	///		- Cannot use auth object within Cypress file without an invalid-api-key error
 });
