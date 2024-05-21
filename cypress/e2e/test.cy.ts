102f6b0a93f30ca5487b75c8425867be77c895e0
Jeloves <anjelovaleraecube@icloud.com>
2024-05-21 16:48:16 -0400

    Wrote function to clear firebase emulator data

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,21 +1,17 @@
-import { createUser } from "@/firebase/auth";
-import { createBudget } from "@/firebase/budgets";
-import { firestore } from "@/firebase/firebase.config";
-import { connectAuthEmulator, createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import firebase from "firebase/compat/app";
-import { connectFirestoreEmulator } from "firebase/firestore";
+import { clearEmulators } from "cypress/support/firebase";
+
 
 describe("Firestore Emulator Tests", () => {
 	let baseURL: string;
-  
+
 	before(() => {
 		baseURL = Cypress.env("BASE_URL");
 		cy.visit(baseURL);
 	});
 
-
-	it("visits the webpage", () => {
-		
+	after(() => {
+		clearEmulators();
 	});
-});
 
+	it("visits the webpage", () => {});
+});
