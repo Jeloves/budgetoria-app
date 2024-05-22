e84b09fa09b81200c8b5a7c5139a160be32cb4e7
Jeloves <anjelovaleraecube@icloud.com>
2024-05-22 15:23:37 -0400

    wrote tests for moving subcategories

--- cypress/e2e/edit-page/edit-page.cy.ts
+++ cypress/e2e/edit-page/edit-page.cy.ts
@@ -141,7 +141,7 @@ describe("Editing Categories Test", () => {
 			cy.get('[data-test-id="topbar"] button').eq(2).click();
 		});
 
-		it("Creates individual categories", () => {
+		it("creates individual categories", () => {
 			// Creating all but the first mock.category
 			for (let i = 1; i < mock.categories.length; i++) {
 				cy.get("header button").eq(0).click();
@@ -169,7 +169,7 @@ describe("Editing Categories Test", () => {
 			}
 		});
 
-		it("Creates individual subcategories", () => {
+		it("creates individual subcategories", () => {
 			// Creating subcategories for all but the first mock.category
 			for (let i = 1; i < mock.categories.length; i++) {
 				const filteredSubcategories = mock.subcategories.filter((subcategory) => subcategory.categoryID === mock.categories[i].id);
@@ -185,18 +185,20 @@ describe("Editing Categories Test", () => {
 			}
 
 			const expectedSubcategories: Subcategory[] = [];
-			for (let i=1; i<mock.categories.length; i++) {
+			for (let i = 1; i < mock.categories.length; i++) {
 				const category = mock.categories[i];
-				const filteredSubcategories = mock.subcategories.filter(subcategory => subcategory.categoryID === category.id);
+				const filteredSubcategories = mock.subcategories.filter((subcategory) => subcategory.categoryID === category.id);
 				for (let subcategory of filteredSubcategories) {
-					expectedSubcategories.push(subcategory)
+					expectedSubcategories.push(subcategory);
 				}
 			}
 
-			for (let i=0; i<expectedSubcategories.length; i++) {
-				cy.get('[data-test-id="edit-subcategory-item"').eq(i).within(() => {
-					cy.get("input").should("have.value", expectedSubcategories[i].name);
-				})
+			for (let i = 0; i < expectedSubcategories.length; i++) {
+				cy.get('[data-test-id="edit-subcategory-item"')
+					.eq(i)
+					.within(() => {
+						cy.get("input").should("have.value", expectedSubcategories[i].name);
+					});
 			}
 
 			// Returns to budget-page
@@ -210,6 +212,139 @@ describe("Editing Categories Test", () => {
 						cy.get("span").eq(0).contains(expectedSubcategories[i].name);
 					});
 			}
+		});
+
+		it("creates a category with nested subcategories", () => {
+			const categoryToCreate = mock.categories[0];
+			const subcategoriesToCreate = mock.subcategories.filter((subcategory) => subcategory.categoryID === categoryToCreate.id);
+
+			// Creating the category
+			cy.get("header button").eq(0).click();
+			cy.get('[data-test-id="category-template"] input').type(categoryToCreate.name + "{enter}");
+
+			// Creating its subcategories
+			for (let subcategory of subcategoriesToCreate) {
+				cy.get('[data-test-id="edit-category-item"]')
+					.eq(0)
+					.within(() => {
+						cy.get("button").eq(1).click();
+					});
+				cy.get('[data-test-id="subcategory-template"] input').type(subcategory.name + "{enter}");
+			}
+
+			// Checks edit page for all correct categories & subcategories
+			for (let i = 0; i < mock.categories.length; i++) {
+				cy.get('[data-test-id="edit-category-item"]')
+					.eq(i)
+					.within(() => {
+						cy.get("input").should("have.value", mock.categories[i].name);
+					});
+			}
+
+			const expectedSubcategories: Subcategory[] = [];
+			for (let i = 0; i < mock.categories.length; i++) {
+				const category = mock.categories[i];
+				const filteredSubcategories = mock.subcategories.filter((subcategory) => subcategory.categoryID === category.id);
+				for (let subcategory of filteredSubcategories) {
+					expectedSubcategories.push(subcategory);
+				}
+			}
+			for (let i = 0; i < expectedSubcategories.length; i++) {
+				cy.get('[data-test-id="edit-subcategory-item"]')
+					.eq(i)
+					.within(() => {
+						cy.get("input").should("have.value", expectedSubcategories[i].name);
+					});
+			}
+
+			// Returns to budget-page
+			cy.get("header button").eq(1).click();
+
+			// Checks the budget page is updated (thus, firebase is updated)
+			for (let i = 0; i < mock.categories.length; i++) {
+				cy.get('[data-test-id="category-item"]')
+					.eq(i)
+					.within(() => {
+						cy.get("span").eq(0).contains(mock.categories[i].name);
+					});
+			}
+			for (let i = 0; i < expectedSubcategories.length; i++) {
+				cy.get('[data-test-id="subcategory-item"]')
+					.eq(i)
+					.within(() => {
+						cy.get("span").eq(0).contains(expectedSubcategories[i].name);
+					});
+			}
+		});
+	});
+
+	context("Moving subcategories", () => {
+		beforeEach(() => {
+			cy.visit(url + "/budget");
+			cy.get('[data-test-id="topbar"] button').eq(2).click();
+		});
+
+		it('moves "Food" from "Essential" to "Nonessential"', () => {
+			const oldIndex = 0;
+			const newIndex = 2;
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(oldIndex)
+				.within(() => {
+					cy.get("button").eq(1).click();
+				});
+			cy.get('[data-test-id="category-selection-1"]').click();
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(newIndex)
+				.within(() => {
+					cy.get("input").should("have.value", "Food");
+				});
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(oldIndex)
+				.within(() => {
+					cy.get("input").should("not.have.value", "Food");
+				});
+		});
+
+		it('moves "Video Games" from "Nonessential" to "Subscriptions"', () => {
+			const oldIndex = 4;
+			const newIndex = 7;
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(oldIndex)
+				.within(() => {
+					cy.get("button").eq(1).click();
+				});
+			cy.get('[data-test-id="category-selection-2"]').click();
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(newIndex)
+				.within(() => {
+					cy.get("input").should("have.value", "Video Games");
+				});
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(oldIndex)
+				.within(() => {
+					cy.get("input").should("not.have.value", "Video Games");
+				});
+		});
+
+		it('moves "Crunchyroll" from "Subscriptions" to "Essential"', () => {
+			const oldIndex = 5;
+			const newIndex = 0;
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(oldIndex)
+				.within(() => {
+					cy.get("button").eq(1).click();
+				});
+			cy.get('[data-test-id="category-selection-0"]').click();
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(newIndex)
+				.within(() => {
+					cy.get("input").should("have.value", "Crunchyroll");
+				});
+			cy.get('[data-test-id="edit-subcategory-item"]')
+				.eq(oldIndex)
+				.within(() => {
+					cy.get("input").should("not.have.value", "Crunchyroll");
+				});
 		});
 	});
 });
