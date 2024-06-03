bbeb5c070d70ed772265e2a2c31b8871f6b10ecc
Jeloves <anjelovaleraecube@icloud.com>
2024-06-03 06:29:20 -0400

    started component tests for account-transactions-subpage

--- cypress/component/accounts-page/account-transactions-subpage.cy.tsx
+++ cypress/component/accounts-page/account-transactions-subpage.cy.tsx
@@ -1,14 +1,23 @@
 import { AccountTransactionsSubpage } from "@/features/accounts-page/account-transactions-subpage/account-transactions-subpage";
 import { getMockData } from "../../support/mock";
+import { createTransaction } from "@/firebase/transactions";
+import { v4 as uuidv4 } from "uuid";
 
 describe("<AccountTransactionsSubpage />", () => {
 	const mock = getMockData();
 	const budgetData = {
-		userID: "",
+		userID: uuidv4(),
 		budgetID: mock.budget.id,
 		year: 2024,
 		month: 2,
 	};
+
+    before(() => {
+        // Creates transaction docs in firebase
+        for (const transaction of mock.transactions) {
+            createTransaction(budgetData.userID, budgetData.budgetID, transaction);
+        }
+    })
 
 	context("All Accounts", () => {
 		beforeEach(() => {
@@ -24,5 +33,9 @@ describe("<AccountTransactionsSubpage />", () => {
 				/>
 			);
 		});
+
+        it("shows 'All Accounts' as header text", () => {
+            cy.get("header span").should("have.text", "All Accounts")
+        })
 	});
 });
